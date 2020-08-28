a81d34785c9f45ad1533093abb7330ce997cbc10
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-08-27 22:28:18 -0300

    web_dev

---
+++ exercicios-js/comentarios.js
@@ -0,0 +1,16 @@
+// Comentários de uma linha
+console.log("Linha 1")
+
+/*
+Comentários de
+múltiplas linhas
+ */
+
+console.log('Linha 2')
+
+/*
+* Comentários de várias
+* linhas
+* buscando eficiência
+*/
+console.log('Linha 3')
