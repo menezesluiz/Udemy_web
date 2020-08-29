1d55e362ce772112e28373904c5600d1d7b812db
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-08-29 16:27:26 -0300

    web_dev

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // JavaScript fracamente tipada, retorna esse valor anormal
+console.log('10' / 2)
+console.log(0.1 + 0.7)
+console.log('Show!' * 2)
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
